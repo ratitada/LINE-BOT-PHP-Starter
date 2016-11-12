d396011f0ccbf07c678ce94cc73c64394193941e
Rati Watanatada <ratiwatanatada@hotmail.com>
2016-11-12 23:20:48 +0700

    reply a balloon instead of 2 balloons
    still confuse with 2 balloons reply.

--- bot.php
+++ bot.php
@@ -19,16 +19,10 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			if ($text == 'cpu') {
 				$reply = 'cpu\'s status';
-				$messages = [
-					{
-						'type' => 'text',
-						'text' => $reply
-					},
-					{
-						'type' => 'image',
-						'originalContentUrl' => 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg',
-						'previewImageUrl'=> 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg'
-					}
+				$messages = [					
+					'type' => 'image',
+					'originalContentUrl' => 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg',
+					'previewImageUrl'=> 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg'
 				];
 			}
 
